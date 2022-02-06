92cebdb02c5477d921b3d3821d478f4706afedce
Josue_Lopez <josuedlopez1982@gmail.com>
2022-02-06 14:11:45 -0300

    Change Login.js feat state y funciones para el componente

--- cliente/src/components/auth/Login.js
+++ cliente/src/components/auth/Login.js
@@ -1,16 +1,40 @@
-
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const Login = () => {
+    // state de inicio de sesión
+    const [user, saveUser] = useState({
+        email: '',
+        password: ''
+    })
 
-    const onChange = () => {
+    // extraer de usuario
+    const {email, password} = user
 
+    const onChange = e => {
+        saveUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // cuando el usuario quiere iniciar sesión
+    const onSubmit = e => {
+        e.preventDefault()
+
+        // validar que no haya campos vacíos
+
+
+        // pasarlo al action
     }
 
     return (
         <div className="form-usuario">
             <div className="contenedor-form sombra-dark">
                 <h1>Iniciar Sesión</h1>
-                <form>
+                <form
+                    onSubmit={onSubmit}
+                >
 
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
@@ -19,6 +43,7 @@ const Login = () => {
                             id="email"
                             name="email"
                             placeholder="Ingresa tu email"
+                            value={email}
                             onChange={onChange}
                         />
                     </div>
@@ -30,6 +55,7 @@ const Login = () => {
                             id="password"
                             name="password"
                             placeholder="Ingresa tu password"
+                            value={password}
                             onChange={onChange}
                         />
                     </div>
@@ -43,6 +69,11 @@ const Login = () => {
                     </div>
 
                 </form>
+
+                <Link to={'NewAccount'} className="enlace-cuenta">
+                    ¿No tienes cuenta? Crea una Aquí
+                </Link>
+
             </div>
         </div>
     )
