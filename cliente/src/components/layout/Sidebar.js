162b8df997e1f90243419bc247e954ab094eb38d
Josue_Lopez <josuedlopez1982@gmail.com>
2022-02-06 16:23:09 -0300

    Add Sidebar
    Se crea componente Sidebar.js [Change Projects.js se crea la estructura principal y se añade  <Sidebar/>

---
+++ cliente/src/components/layout/Sidebar.js
@@ -0,0 +1,15 @@
+
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <h1>MERN<samp>Tasks</samp></h1>
+
+            <div className="proyectos">
+                <h2>Tus Proyectos</h2>
+            </div>
+        </aside>
+    )
+}
+
+export default Sidebar
--- cliente/src/components/projects/Projects.js
+++ cliente/src/components/projects/Projects.js
@@ -1,11 +1,31 @@
+import Sidebar from "../layout/Sidebar"
 
 
 
 const Projects = () => {
     return (
-        <h1>
-            Desde Projects.js
-        </h1>
+        <>
+
+            <div className="contenedor-app">
+
+                <Sidebar/>
+
+            </div>
+
+            <div className="seccion-principal">
+
+                <main>
+
+                    <div className="contenedor-tareas">
+
+                    </div>
+
+                </main>
+
+            </div>
+
+        </>
+
     )
 }
 
