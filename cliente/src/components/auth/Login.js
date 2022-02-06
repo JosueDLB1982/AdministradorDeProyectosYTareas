3b9613f8691ed75b84a913f7713e6e4b6a1859d9
Josue_Lopez <josuedlopez1982@gmail.com>
2022-02-06 13:47:13 -0300

    Change Login.js
    Feat Inicia diseño de formulario de login

--- cliente/src/components/auth/Login.js
+++ cliente/src/components/auth/Login.js
@@ -1,10 +1,50 @@
 
 
 const Login = () => {
+
+    const onChange = () => {
+
+    }
+
     return (
-        <h1>
-            Desde Login.js
-        </h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+                <form>
+
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Ingresa tu email"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Ingresa tu password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesión"
+                        />
+                    </div>
+
+                </form>
+            </div>
+        </div>
     )
 }
 
